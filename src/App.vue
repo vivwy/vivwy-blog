71385fec54ac071b526c3778b5cb187491dec2d8
Snow Vivwy <snowvivwy@outlook.com>
2025-12-06 00:04:18 +0800

    feat: initialize project - vivwy blog

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const msg = ref('You did it!');
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the documentation</p>
+  <p>{{ msg }}</p>
+</template>
+
+<style scoped></style>
